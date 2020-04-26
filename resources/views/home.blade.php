839cd29a96ad0a3b8f74b29748de2b5a241d6783
Tomas Talalas <44781061+realVato@users.noreply.github.com>
2020-04-26 16:37:22 +0300

    photo, desc & post rows

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,38 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img style="height: 120px" src="https://instagram.fkun1-1.fna.fbcdn.net/v/t51.2885-19/s320x320/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fkun1-1.fna.fbcdn.net&_nc_ohc=2AbikMRp0lIAX8Xccsg&oh=cba760456c6f7ed9566510ac94c3ccab&oe=5ECE19BE" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>laraGi</h1>
             </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>409</strong> posts</div>
+                <div class="pr-5"><strong>1m</strong> followers</div>
+                <div class="pr-5"><strong>0</strong> following</div>
+            </div>
+            <br>
+            <div>
+                <strong><a href="https://tomastalalas.netlify.app/" target="_blank" style="text-decoration: none; color: #000;">tomastalalas.netlify.app</a></strong> 
+            </div>
+            <div>
+                <p>My super amazing, simple and powerful portofolio.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fkun1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/88374782_543733416501650_6638052850750298038_n.jpg?_nc_ht=instagram.fkun1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=r-uYT77JhMIAX8s4inC&oh=03daf9586997d83b290295a85d51cb0f&oe=5ECE83B7" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fkun1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.109.925.925a/s640x640/90429603_618090275709978_2696911208620941912_n.jpg?_nc_ht=instagram.fkun1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=Hf_jABA1rQMAX_3n1Pz&oh=242263c9c0e0655ed255c24d377d6d93&oe=5ECDF48D" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fkun1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c1.0.747.747a/s640x640/85221994_174215494029992_4418944792110903657_n.jpg?_nc_ht=instagram.fkun1-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=98QrzKBpSFMAX_DdkE2&oh=07a45af99842539078677f8b894f96ca&oe=5ECD1C3C" class="w-100">
         </div>
     </div>
 </div>
